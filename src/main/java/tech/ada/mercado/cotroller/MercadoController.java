3e05b69ed2db2014f7cf6902acdd3f6b572b9e60
Andre de Camargo <camargo_and@hotmail.com>
2023-04-19 17:20:22 -0300

    Exercicio da Aula WebIII CRUD

---
+++ src/main/java/tech/ada/mercado/cotroller/MercadoController.java
@@ -0,0 +1,58 @@
+package tech.ada.mercado.cotroller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import tech.ada.mercado.model.Mercado;
+import tech.ada.mercado.service.MercadoService;
+
+@RestController
+@Slf4j
+@RequestMapping("/mercados")
+public class MercadoController {
+
+    private MercadoService service;
+
+    public MercadoController(MercadoService service){
+        this.service = service;
+    }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<ResponseEntity<Mercado>> save(@RequestBody Mercado mercado){
+        return service.save(mercado).map(m -> ResponseEntity.ok().body(m));
+    }
+
+    @PutMapping("/id")
+    public Mono<ResponseEntity<Mercado>> update(@RequestBody Mercado mercado, @RequestParam String id){
+        return service.update(mercado, id)
+                .map(m -> ResponseEntity.ok().body(m))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.FOUND)
+    public Mono<ResponseEntity<Flux<Mercado>>> findAll(){
+        return service.findAll()
+                .collectList()
+                .map(m -> ResponseEntity.ok().body(Flux.fromIterable(m)))
+                .switchIfEmpty(Mono.just(ResponseEntity.noContent().build()));
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Mercado>> findById(@PathVariable String id){
+        return service.findById(id)
+                .map(m -> ResponseEntity.ok().body(m))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()));
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Void>> delete(@PathVariable String id){
+        return service.delete(id)
+                .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                .switchIfEmpty(Mono.just(ResponseEntity.notFound().build()))
+                .onErrorResume(e -> Mono.just(ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()));
+    }
+}
---
+++ src/main/java/tech/ada/mercado/service/MercadoService.java
@@ -0,0 +1,43 @@
+package tech.ada.mercado.service;
+
+import com.mongodb.internal.connection.IndexMap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import tech.ada.mercado.model.Mercado;
+import tech.ada.mercado.repository.MercadoRepository;
+
+@Service
+public class MercadoService {
+    private MercadoRepository repository;
+
+    public MercadoService(MercadoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Mono<Mercado> save(Mercado mercado) {
+        return repository.save(mercado);
+    }
+
+    public Flux<Mercado> findAll() {
+        return repository.findAll();
+    }
+
+
+    public Mono<Mercado> findById(String id) {
+        return repository.findById(id);
+    }
+
+    public Mono<?> delete(String id) {
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("user not found id " + id)))
+                .flatMap(m -> repository.deleteById(id))
+                .then();
+    }
+
+    public Mono<Mercado> update(Mercado mercado, String id) {
+        return this.findById(id)
+                .flatMap( m -> repository.save(m.update(mercado)));
+    }
+}
